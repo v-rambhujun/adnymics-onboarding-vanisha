2f315e15d1022f28da0bb5b70df6e705bb047732
Vanisha Rambhujun <v.rambujun@adnymics.com>
2019-12-05 11:28:32 +0100

    Updated version of the Bash exercises

--- Bash/Exercises.sh
+++ Bash/Exercises.sh
@@ -1,18 +1,36 @@
+#####################################################################
 # Exercise 2 
 # Find where the file abc.py is located on your file system
 locate -b '\abc.py'
 
+#####################################################################
 # Exercise 3
-# Create a file that contains a count from 1 to 1000000 (one number per line), and print then number of lines the files contains
+# Create a file that contains a count from 1 to 1000000 (one number 
+# per line), and print then number of lines the files contains
 seq -w  1 1000000 > number.txt && wc -l < number.txt
 
+#####################################################################
 # Exercise 4
 # Same file as in Exercise 3, but in revers order 1000000 to 1
-for i in {1000000..1}; do echo $i; done > number.txt && wc -l < number.txt
+seq -w  100 -1 1 > rebmun.txt && wc -l < rebmun.txt
+# or
 
+# However here, the output is a string
+for i in {1000000..1}; do echo $i; done > rebmun.txt && wc -l < rebmun.txt
+
+#####################################################################
 # Exercise 5
 # Print the second to last directory from PATH environment variable
 echo $PATH | awk -F: '{print $(NF-2)}'
 
+#####################################################################
 # Exercise 6
-#
+# Use file created in Exercise 3 and count how many numbers can be 
+# divided by 777 without remainder
+cat number.txt | awk -F" " '{if ($1 % 777 ==0) {counter+=1}} END {print counter}'
+
+#####################################################################
+# Exercise 7
+# Print the most command that is used with "sudo" from each user from 
+# log file: /var/log/auth.log. input
+
